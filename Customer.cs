29473639fd7b6899791849587d3bb07c67cf8773
Avinash Bhardwaj <avinash.bhardwaj@cengage.com>
2024-05-28 13:25:24 +0530

    Initial commit

---
+++ Customer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    public class Customer
+    {
+        List<int> positions;
+        public void SortArray()
+        {
+            int temp=0;
+            int[] arr = new int[] { 20, 65, 98, 71, 64, 11, 2, 80, 5, 6, 100, 50, 13, 9, 80, 454 };
+            positions = new List<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i+1; j<arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+                positions.Add(arr[i]);
+
+                //Console.WriteLine(arr[i]);
+            }
+
+            for (int k = 0; k < positions.Count; k++)
+            {
+                Console.WriteLine(positions[k]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HelloWorld
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hello World
+            Console.WriteLine("Hello World");
+            Console.WriteLine("\n");
+            Console.WriteLine("Boxing");
+
+            int numBoxing = 2024;
+
+            //Boxing
+            object objBoxing = numBoxing;
+
+            numBoxing = 200;
+
+            System.Console.WriteLine("Value - type value of num is : {0}", numBoxing);
+            System.Console.WriteLine("Object - type value of obj is : {0}", objBoxing);
+
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Unboxing");
+
+            int numUnboxing = 2024;
+
+            // boxing
+            object objUnboxing = numUnboxing;
+
+            // unboxing
+            int i = (int)objUnboxing;
+
+            // Display result
+            Console.WriteLine("Value of ob object is : " + objUnboxing);
+            Console.WriteLine("Value of i is : " + i);
+
+        }
+    }
+}
